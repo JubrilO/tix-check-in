7a219ddea1e52520a5ec88bb3bf9a06878f68f47
Jubril O <jubrylola@gmail.com>
2018-11-18 16:35:08 +0100

    Initial Commit

---
+++ tix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tix
+//
+//  Created by Jubril on 18/11/2018.
+//  Copyright © 2018 Tix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
